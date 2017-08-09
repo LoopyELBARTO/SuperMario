d807e153e09bc065f7e28644278cc60f61984e77
loopyelbarto <gundam007>
2017-08-08 23:41:26 -0400

    Added Hud

--- core/src/com/nublarobert/supermario/Screens/PlayScreen.java
+++ core/src/com/nublarobert/supermario/Screens/PlayScreen.java
@@ -5,35 +5,61 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.nublarobert.supermario.Scences.Hud;
 import com.nublarobert.supermario.SuperMario;
 
 public class PlayScreen implements Screen {
     private SuperMario game;
-    Texture texture;
 
     private OrthographicCamera gameCam;
     private FitViewport gamePort;
+    private Hud hud;
+
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
 
     public PlayScreen(SuperMario game){
         this.game = game;
-        texture = new Texture("badlogic.jpg");
         gameCam = new OrthographicCamera();
-        gamePort = new FitViewport(800,480,gameCam);
+        gamePort = new FitViewport(SuperMario.V_WIDTH,SuperMario.V_HIEGHT,gameCam);
+        hud = new Hud(game.batch);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gameCam.position.set(gamePort.getWorldWidth()/2, gamePort.getWorldHeight()/2,0);
     }
     @Override
     public void show() {
 
     }
+    public void  handleInput(float dt){
+        if(Gdx.input.isTouched())
+            gameCam.position.x += 100 * dt;
+    }
+
+    public void update(float dt){
+        handleInput(dt);
+
+        gameCam.update();
+        renderer.setView(gameCam);
+    }
 
     @Override
     public void render(float delta) {
+        update(delta);
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.setProjectionMatrix(gameCam.combined);
-        game.batch.begin();
-        game.batch.draw(texture, 0, 0);
-        game.batch.end();
+
+        renderer.render();
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
     }
 
     @Override
--- desktop/src/com/nublarobert/supermario/desktop/DesktopLauncher.java
+++ desktop/src/com/nublarobert/supermario/desktop/DesktopLauncher.java
@@ -6,7 +6,10 @@ import com.nublarobert.supermario.SuperMario;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1200;
+		config.height = 624;
 		new LwjglApplication(new SuperMario(), config);
 	}
 }
